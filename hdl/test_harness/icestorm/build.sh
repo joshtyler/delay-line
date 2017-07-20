7b5262d622c31c408e3adaef6f1402604eab4442
Joshua Tyler <josh.tyler@btinternet.com>
2017-07-20 19:42:23 +0100

    Cleaned up

--- hdl/test_harness/icestorm/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SRC_DIR=../src
-COMMON_SRC_DIR=../../common/src
-
-yosys -p "synth_ice40 -blif test_harness.blif" $SRC_DIR/test_harness_wrapper.v $SRC_DIR/controller.v $SRC_DIR/demodulator.v $SRC_DIR/mem_manager.v $SRC_DIR/modulator.v $SRC_DIR/msg_asm.v $SRC_DIR/msg_disasm.v $SRC_DIR/pll.v $SRC_DIR/replace_num_mem.v $SRC_DIR/shift_register.v $SRC_DIR/test_harness.v $SRC_DIR/uart_rx.v $SRC_DIR/uart_tx.v $COMMON_SRC_DIR/power_on_reset.v $COMMON_SRC_DIR/fifo.v $COMMON_SRC_DIR/dual_port_ram.v
-
-
--- hdl/test_harness/icestorm/timing_test/test.sh
+++
@@ -1,2 +0,0 @@
-iceunpack ../../iCEcube/test_harness/test_harness_Implmnt/sbt/outputs/bitmap/test_harness_wrapper_bitmap.bin test.asc
-icetime -tmd hx1k test.asc
