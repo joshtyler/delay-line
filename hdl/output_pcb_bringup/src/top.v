fda2319b4ef2df94c3da95a48fb2c247f90b28c3
Joshua Tyler <josh.tyler@btinternet.com>
2018-09-10 19:28:09 +0100

    Add output pcb bringup

--- hdl/output_pcb_bringup/src/top.v
+++ hdl/output_pcb_bringup/src/top.v
@@ -1,11 +1,16 @@
 // Top level module
 
-module top(LED0, LED1);
+module top(LED0, LED1, clk_in);
 
+input clk_in;
 output LED0, LED1;
+
+reg [23:0] ctr;
 
 //Hold LEDs
 assign LED0 = 0;
-assign LED1 = 1;
+assign LED1 = ctr[23];
+
+always @(posedge clk_in) ctr <= ctr + 1;
 
 endmodule
